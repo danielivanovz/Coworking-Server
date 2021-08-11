d6d68d1d9171c4c2f231d639201f11d31450b92f
Daniel Ivanov <daniel.ivanov@mavigex.com>
2021-08-11 18:14:52 +0200

    implemented express server, db connection and logger

---
+++ src/db/index.ts
@@ -0,0 +1,43 @@
+import { MongoClient, Db, Collection } from "mongodb";
+import log from "../logger";
+import env from "../env";
+
+interface MongoOptions {
+  readonly useUnifiedTopology: boolean;
+  readonly ignoreUndefined?: boolean;
+}
+
+const options: MongoOptions = {
+  useUnifiedTopology: true,
+  ignoreUndefined: true,
+};
+
+const mongoURI: string = env.getDBUri();
+
+export const establishConnection = async (dbName: string, dbCollection: string) => {
+  const client = new MongoClient(mongoURI, options);
+  try {
+    console.log(dbName);
+    console.log(dbCollection);
+
+    const connected = await client
+      .connect()
+      .finally(() =>
+        log.info("Connection to the database established - status: " + client["topology"].s.state)
+      );
+
+    console.log();
+
+    db = connected.db(dbName);
+
+    collection = db.collection(dbCollection);
+  } catch (error) {
+    log.error;
+  }
+
+  return client;
+};
+
+export let db: Db;
+
+export let collection: Collection;
