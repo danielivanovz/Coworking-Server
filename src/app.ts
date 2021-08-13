7dd38472269abf2e120dad92be5bd61924c91182
Daniel Ivanov <daniel.ivanov@mavigex.com>
2021-08-13 18:47:28 +0200

    additional deploy issues

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,12 @@ import log from "./logger";
 
 const startServer = async () => {
 	const app = createServer();
-	const HOST: string = env.getHost();
-	const PORT: number = env.getPort();
 
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
 
-	app.listen(PORT, () => {
-		log.info(`Server is running at http://${HOST}:${PORT}`);
+	app.listen(8090, () => {
+		log.info(`Server is running at :8090`);
 	});
 
 	const dbName: string = env.getDBName();
