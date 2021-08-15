978dec406641cc1a7090cd0e68095c0595a1996a
Daniel Ivanov <daniel.ivanov@mavigex.com>
2021-08-16 00:24:46 +0200

    `workspace` routes updated

--- src/routes/v1/workspace.r.ts
+++ src/routes/v1/workspace.r.ts
@@ -8,4 +8,8 @@ router.get("/list", workspaceController.getWorkspaces);
 router.get("/:query", workspaceController.getWorkspacesByQuery);
 router.get("/:location/:name", workspaceController.getWorkspacesByCityAndName);
 
+router.post("/", workspaceController.addWorkspace);
+router.put("/", workspaceController.updateWorkspace);
+router.delete("/", workspaceController.deleteWorkspace);
+
 export default router;
