cf9fe3318530647a91e31cb95014bb0e89f4717c
Daniel Ivanov <daniel.ivanov@mavigex.com>
2021-08-15 23:23:16 +0200

    added `post`, `put`, `delete` endpoint to `space`

--- src/controllers/space.c.ts
+++ src/controllers/space.c.ts
@@ -3,7 +3,7 @@ import { db } from "../db";
 import env from "../env";
 import log from "../logger";
 import { Collections } from "../types";
-import { ObjectId } from "mongodb";
+import { ObjectId, ReturnDocument } from "mongodb";
 import { Space } from "../models";
 
 export const getSpace = async (req: Request, res: Response) => {
@@ -21,8 +21,6 @@ export const getSpace = async (req: Request, res: Response) => {
 
 export const getSpaceByID = async (req: Request, res: Response) => {
 	try {
-		console.log(req.query);
-
 		const response = await db
 			.collection(env.getCollection(Collections.SPACE_COLLECTION))
 			.findOne(new ObjectId(<string>req.query.id));
@@ -33,5 +31,48 @@ export const getSpaceByID = async (req: Request, res: Response) => {
 			.end(JSON.stringify(<Space>response));
 	} catch (error) {
 		log.error("Error finding object by ID", error);
+	}
+};
+
+export const addSpace = async (req: Request, res: Response) => {
+	try {
+		const response = await db
+			.collection(env.getCollection(Collections.SPACE_COLLECTION))
+			.insertOne(req.body);
+
+		res
+			.setHeader("Content-type", "application/json")
+			.status(200)
+			.end(JSON.stringify(response.insertedId));
+	} catch (error) {
+		log.error("Error creating new space");
+	}
+};
+
+export const deleteSpace = async (req: Request, res: Response) => {
+	try {
+		const response = await db
+			.collection(env.getCollection(Collections.SPACE_COLLECTION))
+			.findOneAndDelete({ _id: new ObjectId(<string>req.body.id) });
+
+		res.setHeader("Content-type", "application/json").status(200).end(JSON.stringify(response.ok));
+	} catch (error) {
+		log.error("Error deleting new space");
+	}
+};
+
+export const updateSpace = async (req: Request, res: Response) => {
+	try {
+		const response = await db
+			.collection(env.getCollection(Collections.SPACE_COLLECTION))
+			.findOneAndUpdate(
+				{ _id: new ObjectId(<string>req.body.id) },
+				{ $set: req.body },
+				{ returnDocument: ReturnDocument.AFTER, projection: { _id: 0 } }
+			);
+
+		res.setHeader("Content-type", "application/json").status(200).end(JSON.stringify(response.ok));
+	} catch (error) {
+		log.error("Error deleting new space");
 	}
 };
--- src/routes/v1/space.r.ts
+++ src/routes/v1/space.r.ts
@@ -7,4 +7,8 @@ const router = Router();
 router.get("/list", spaceController.getSpace);
 router.get("/retrieve", spaceController.getSpaceByID);
 
+router.post("/", spaceController.addSpace);
+router.put("/", spaceController.updateSpace);
+router.delete("/", spaceController.deleteSpace);
+
 export default router;
