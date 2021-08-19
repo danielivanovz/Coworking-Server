80a46bbf3f07f4c9e2e317e09fd12a745f9cc8ee
Daniel Ivanov <daniel.ivanov@mavigex.com>
2021-08-19 10:28:39 +0200

    cors update

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,14 @@ export default function createServer() {
 		write: (message) => log.info(message),
 	};
 
+	var allowCrossDomain = function (req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+		res.header("Access-Control-Allow-Headers", "Content-Type");
+		next();
+	};
+
+	app.use(allowCrossDomain);
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
 	app.use(helmet());
