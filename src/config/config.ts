ad02bba07c05dae6ad39699fad601b38af8330eb
Daniel Ivanov <daniel.ivanov@mavigex.com>
2021-08-25 21:26:51 +0200

    mw options refactor

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,17 +3,44 @@ import { establishConnection } from "../db";
 import log from "../logger";
 import * as dotenv from "dotenv";
 import { env } from "./env";
+import session from "express-session";
+import morgan from "morgan";
+import { IncomingMessage, ServerResponse } from "http";
 dotenv.config();
+
+interface morganConfiguraton {
+	format: string;
+	options: morgan.Options<IncomingMessage, ServerResponse>;
+}
 
 class ServerConfiguration {
 	HOST: string;
 	PORT: number;
 	dbName: string;
+	sessionOptions: session.SessionOptions;
+	morganOptions: morganConfiguraton;
 
 	constructor() {
 		this.HOST = env.getHost();
 		this.PORT = env.getPort();
 		this.dbName = env.getDBName();
+		this.sessionOptions = {
+			secret: process.env.SECRET_TOKEN,
+			resave: true,
+			saveUninitialized: true,
+		};
+		this.morganOptions = {
+			format: ":method :url :status :res[content-length] - :response-time ms",
+			options: { stream: { write: (message: string) => log.info(message) } },
+		};
+	}
+
+	getSessionOptions() {
+		return this.sessionOptions;
+	}
+
+	getMorganOptions() {
+		return this.morganOptions;
 	}
 
 	async startServer(app: Application) {
--- src/config/mw.ts
+++ src/config/mw.ts
@@ -8,30 +8,25 @@ import session from "express-session";
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import * as dotenv from "dotenv";
+import { serverConfiguration } from ".";
 dotenv.config();
 
 export default function mw(app: Application) {
-	const stream: morgan.StreamOptions = {
-		write: (message: string) => log.info(message),
-	};
-
-	app.use(cors({ credentials: true }));
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: false }));
-
-	app.use(helmet());
 	app.use(
-		session({
-			secret: process.env.SECRET_TOKEN,
-			resave: true,
-			saveUninitialized: true,
-		})
+		cors({ credentials: true }),
+		express.json(),
+		express.urlencoded({ extended: false }),
+		helmet(),
+		session(serverConfiguration.sessionOptions),
+		cookieParser(),
+		morgan(
+			serverConfiguration.getMorganOptions().format,
+			serverConfiguration.getMorganOptions().options
+		),
+		passport.initialize(),
+		passport.session(),
+		routes
 	);
-	app.use(cookieParser());
-	app.use(morgan(":method :url :status :res[content-length] - :response-time ms", { stream }));
-	app.use(passport.initialize());
-	app.use(passport.session());
-	app.use(routes);
 
 	return app;
 }
