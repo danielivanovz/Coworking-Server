2fe94ed10a5b70f58bd986b65daaca0e82f6c8f0
Daniel Ivanov <daniel.ivanov@mavigex.com>
2021-08-26 11:35:00 +0200

    mw typos

--- src/config/mw.ts
+++ src/config/mw.ts
@@ -3,19 +3,11 @@ import express, { Application, NextFunction } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import routes from "../routes";
-
 import cookieParser from "cookie-parser";
-
 import * as dotenv from "dotenv";
 import { serverConfiguration } from ".";
 
 dotenv.config();
-
-export interface UserInterface {
-	id: string;
-	username: string;
-	isAdmin: boolean;
-}
 
 export default function mw(app: Application) {
 	app.use(
